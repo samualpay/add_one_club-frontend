66aa044d571f2f17e3440157ad031715e3b36052
star <star@funpodium.net>
2020-11-30 17:03:09 +0800

    add rmi before buildImage

--- buildImage.sh
+++ buildImage.sh
@@ -1,3 +1,4 @@
+docker rmi samualpay/add_one_club-frontend:latest
 docker build -t samualpay/add_one_club-frontend:latest .
 docker login --username=samualpay
 docker push samualpay/add_one_club-frontend:latest
