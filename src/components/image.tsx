dfaa553c83cb9d0847e4c79ebd3d62352c7b0ab1
star <star@funpodium.net>
2020-12-03 20:02:59 +0800

    upload service

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,17 +1,15 @@
-import {Image} from 'antd'
-import { ImageProps } from 'rc-image';
-import React from 'react';
+import { Image } from "antd";
+import { ImageProps } from "rc-image";
+import React from "react";
 // const IMAGE_PREFIX = '/image/'
-const IMAGE_PREFIX = 'https://img.ruten.com.tw/s4/892/273/ziujrhjk/1/f8/4e/'
-function MyImage({src,...props}:ImageProps){
-    function getSrc(src:string|undefined){
-        if(src){
-            return `${IMAGE_PREFIX}src`
-        }
-        return src
+const IMAGE_PREFIX = "/images/";
+function MyImage({ src, ...props }: ImageProps) {
+  function getSrc(src: string | undefined) {
+    if (src) {
+      return `${IMAGE_PREFIX}${src}`;
     }
-    return (
-        <Image src={getSrc(src)} {...props}></Image>
-    )
+    return src;
+  }
+  return <Image src={getSrc(src)} {...props}></Image>;
 }
-export default MyImage
+export default MyImage;
--- src/service/upload.service.ts
+++ src/service/upload.service.ts
@@ -1,24 +1,37 @@
+import Axios from "axios";
+
 type UploadImageResp = {
   url: string;
 };
 class UploadService {
-  async uploadImage(file: File,
+  async uploadImage(
+    file: File,
     onProgress?: (percent: number) => void
   ): Promise<UploadImageResp> {
-    // todo: uploadImage api
-    if (onProgress) {
-      onProgress(50);
-    }
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          url:
-            "22025708576846_977.jpg",
-        });
-      }, 1000);
-    });
+    const fmData = new FormData();
+    fmData.append("image", file);
+    const response = await Axios.post<{ filename: string }>(
+      "/api/upload/image",
+      fmData,
+      {
+        headers: { "content-type": "multipart/form-data" },
+        onUploadProgress: (event) => {
+          const percent = Math.floor((event.loaded / event.total) * 100);
+          if (onProgress) {
+            onProgress(percent);
+            if (percent === 100) {
+              setTimeout(() => {
+                onProgress(0);
+              }, 100);
+            }
+          }
+        },
+      }
+    );
+    return { url: response.data.filename };
   }
-  async uploadVideo(file: File,
+  async uploadVideo(
+    file: File,
     onProgress?: (percent: number) => void
   ): Promise<UploadImageResp> {
     // todo: uploadVideo api
@@ -28,8 +41,7 @@ class UploadService {
     return new Promise((resolve) => {
       setTimeout(() => {
         resolve({
-          url:
-            "22025708576846_977.jpg",
+          url: "22025708576846_977.jpg",
         });
       }, 1000);
     });
